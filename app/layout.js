f728763a4404d689d37e18c40216997784ba45e6
VeriSeek Dev <veriseek-dev@merakiads.tech>
2026-05-14 13:17:50 +0000

    feat: add Meta Pixel (PageView) to landing page

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import Script from 'next/script';
 import './globals.css';
 
 export const metadata = {
@@ -16,8 +17,36 @@ export default function RootLayout({ children }) {
           href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;600;700&family=DM+Serif+Display&display=swap"
           rel="stylesheet"
         />
+
+        {/* Meta Pixel Code */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1599242251252519');
+            fbq('track', 'PageView');`}
+        </Script>
+        {/* End Meta Pixel Code */}
       </head>
-      <body>{children}</body>
+      <body>
+        {/* Meta Pixel noscript fallback */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1599242251252519&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+
+        {children}
+      </body>
     </html>
   );
 }
