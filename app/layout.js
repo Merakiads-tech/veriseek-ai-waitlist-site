a262e978f649b3f79fb01142932252fed57de35b
VeriSeek Dev <veriseek-dev@merakiads.tech>
2026-05-21 07:26:09 +0000

    chore: update Meta Pixel ID to 1304756355125527

--- app/layout.js
+++ app/layout.js
@@ -28,7 +28,7 @@ export default function RootLayout({ children }) {
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1599242251252519');
+            fbq('init', '1304756355125527');
             fbq('track', 'PageView');`}
         </Script>
         {/* End Meta Pixel Code */}
@@ -40,7 +40,7 @@ export default function RootLayout({ children }) {
             height="1"
             width="1"
             style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=1599242251252519&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1304756355125527&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
